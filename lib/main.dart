5e6f96abecd8371e56bb480c7d2e3ecaa21acc0a
jbri <jonas@rbri.de>
2025-06-19 10:25:04 +0200

    add: basic navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'constants/app_theme.dart';
+import 'widgets/app/main_navigation_widget.dart';
 
 void main() {
   runApp(const ProviderScope(child: MainApp()));
@@ -11,9 +12,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: appTheme,
-      home: const Scaffold(body: Center(child: Text('Hello World!'))),
-    );
+    return MaterialApp(theme: appTheme, home: const MainNavigationWidget());
   }
 }
---
+++ lib/pages/master_data/master_data_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MasterDataPage extends StatelessWidget {
+  const MasterDataPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Master Data'));
+  }
+}
